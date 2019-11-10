74a59aa687374774de0102ddec471ff68a65984e
gpratik <gpratik@vmware.com>
2019-11-09 17:17:17 -0800

    Adding Karma spec reporter, remove boilerplate code
    -- Karma spec reporter, is used to log unit test to console while running ng tests
    -- command> npm install karma-spec-reporter
    
    -- Command to run ng test with spec reporter
    -- command> npm run test --reporters='spec'
    -- This will run the unit tests and log the associated test on the console

--- app-workspace/projects/sample-app/src/app/app.component.spec.ts
+++ app-workspace/projects/sample-app/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sample-app app is running!');
+    expect(compiled.querySelector('span').textContent).toContain('sample-app is running!');
   });
 });
